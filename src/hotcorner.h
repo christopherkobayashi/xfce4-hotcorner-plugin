ce88044ed8f304d2679ce220e40322f4addeff46
christopherkobayashi <christopherkobayashi@users.noreply.github.com>
2022-05-26 14:43:07 +0900

    Change icon.

--- src/hotcorner.h
+++ src/hotcorner.h
@@ -25,7 +25,7 @@
 #include <libwnck/libwnck.h>
 
 #define UPDATE_TIMEOUT 50
-#define HOTCORNER_ICON_NAME "org.xfce.settings.display"
+#define HOTCORNER_ICON_NAME "video-display"
 
 typedef struct HotCorner HotCorner;
 typedef void (* ActionCallback)(int, HotCorner *);
